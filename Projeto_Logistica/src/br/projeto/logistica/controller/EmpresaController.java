fa739316431db45488ed0a50fe13b15856fd4a3e
Gustavo Henrique <gustavohenriqueaj@gmail.com>
2015-06-10 18:46:31 -0300

    .

---
+++ Projeto_Logistica/src/br/projeto/logistica/controller/EmpresaController.java
@@ -0,0 +1,20 @@
+package br.projeto.logistica.controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class EmpresaController implements ActionListener{
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+	
+	
+	
+
+}
